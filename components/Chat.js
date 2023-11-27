e41174217ec064cff8755c0f41b30051b0a24864
nnnzrnk <127133520+nnnzrnk@users.noreply.github.com>
2023-11-27 20:34:26 +0100

    updated chat page

--- components/Chat.js
+++ components/Chat.js
@@ -1,17 +1,71 @@
-import { useEffect } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { useEffect, useState } from "react";
+import { StyleSheet, View, Platform, KeyboardAvoidingView} from "react-native";
+import { Bubble, GiftedChat} from "react-native-gifted-chat";
 
 const Chat = ({ route, navigation }) => {
   const {name} = route.params
   const {background} = route.params;
+  const [messages, setMessages] = useState([])
+
+  // for sending messages
+  const onSend = (newMessages) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+  }
+
+  // for customazing your messages
+  const renderBubble = (props) => {
+    return <Bubble
+    {...props}
+    wrapperStyle={ {
+      right: {
+        backgroundColor: '#2a9d8f', 
+      }, 
+      left: {
+        backgroundColor: '#fff'
+      }
+    }}
+    />
+  }
+
 
   useEffect(() => {
     navigation.setOptions({title: name})
   }, [])
+
+  // default messages that appear when you get to the chat page
+  useEffect(()=> {
+     setMessages([
+      {
+        _id: 1, 
+        text: 'Hello developer', 
+        createdAt: new Date(), 
+        user: {
+          _id: 2,
+          name: 'React Native', 
+          avatar: 'https://placeimg.com/140/140/any'
+        }, 
+      }, 
+      {
+        _id: 2, 
+        text: 'This is a system message', 
+        createdAt: new Date(), 
+        system: true,
+      }, 
+     ])
+  }, [])
   
   return (
     <View style={[styles.container, {backgroundColor: background}]}>
-      <Text>Hello Chats!</Text>
+      <GiftedChat
+      messages={messages}
+      renderBubble={renderBubble}
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: 1
+      }}
+      />
+      {/* so that the keyboard does not overlap the input  */}
+      {Platform.OS === 'android' ? <KeyboardAvoidingView behavior = "height" /> : null}
     </View>
   );
 };
@@ -19,8 +73,6 @@ const Chat = ({ route, navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
   },
 });
 
