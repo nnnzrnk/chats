821d2d676c4ce6223533689198e8dab331737efb
nnnzrnk <127133520+nnnzrnk@users.noreply.github.com>
2023-11-27 01:57:24 +0100

    completed 5.1

---
+++ components/Chat.js
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+const Chat = ({ route, navigation }) => {
+  const {name} = route.params
+  const {background} = route.params;
+
+  useEffect(() => {
+    navigation.setOptions({title: name})
+  }, [])
+  
+  return (
+    <View style={[styles.container, {backgroundColor: background}]}>
+      <Text>Hello Chats!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default Chat;
---
+++ components/Start.js
@@ -0,0 +1,143 @@
+import { View, StyleSheet, Text, TextInput, ImageBackground, TouchableOpacity } from "react-native"
+import Chat from "./Chat"
+import { useState } from "react"
+
+const image = { uri: 'https://i.pinimg.com/564x/e6/27/ac/e627ac9dbda722a8676142a86e78d425.jpg' }
+
+const Start = ({ navigation }) => {
+    const [name, setName] = useState('')
+    const [background, setBackground] = useState('')
+
+    return (
+        <ImageBackground source={image} resizeMode="cover" style={styles.container}>
+            <View style={styles.titleView}>
+                <Text style={styles.appTitle}>App Title</Text>
+            </View>
+            <View style={styles.mainView}>
+                <TextInput style={styles.textInput} value={name} onChangeText={setName} placeholder='Type your username here' />
+
+                {/* View with text and colors container  */}
+                <View>
+                    <Text style={styles.chooseText}>Choose Background Color</Text>
+                {/* only colors container     */}
+                    <View style={styles.colorsContainer}>
+                        <TouchableOpacity
+                            style={[styles.selectStyle, styles.selectColor1]}
+                            onPress={() => setBackground(styles.selectColor1.backgroundColor)}>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            style={[styles.selectStyle, styles.selectColor2]}
+                            onPress={() => setBackground(styles.selectColor2.backgroundColor)}
+                        ></TouchableOpacity>
+                        <TouchableOpacity
+                            style={[styles.selectStyle, styles.selectColor3]}
+                            onPress={() => setBackground(styles.selectColor3.backgroundColor)}
+                        ></TouchableOpacity>
+                        <TouchableOpacity
+                            style={[styles.selectStyle, styles.selectColor4]}
+                            onPress={() => setBackground(styles.selectColor4.backgroundColor)}
+                        ></TouchableOpacity>
+                    </View>
+                </View>
+
+             
+                <TouchableOpacity
+                    style={styles.buttonStart}
+                    onPress={() => navigation.navigate('Chat', { name: name, background: background })}>
+                    <Text style={styles.textButton}>Start Chatting</Text>
+                </TouchableOpacity>
+            </View>
+        </ImageBackground>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-evenly'
+    },
+    titleView: {
+        flex: 2,
+        justifyContent: 'space-around'
+    },
+    appTitle: {
+        fontSize: 45,
+        fontWeight: '600',
+        color: '#ffffff',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    mainView: {
+        alignItems: 'center',
+        backgroundColor: '#ffffff',
+        width: '88%',
+        height: '44%',
+        marginBottom: 30,
+        justifyContent: 'space-evenly',
+        borderRadius: 10,
+    },
+    textInput: {
+        width: '88%',
+        padding: 15,
+        borderWidth: 1,
+        borderColor: '#757083',
+        marginTop: '8%',
+        marginBottom: 15,
+        position: 'absolute',
+        top: 5
+    },
+    chooseText: {
+        fontSize: 18,
+        fontWeight: '300',
+        color: '#757083',
+        opacity: 100,
+        marginBottom: 20
+    },
+    colorsContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        width: '80%',
+        justifyContent: "space-between"
+    },
+    selectStyle: {
+        width: 60,
+        height: 60,
+        borderRadius: '50%',
+
+    },
+    selectColor1: {
+        backgroundColor: '#090C08',
+    },
+    selectColor2: {
+        backgroundColor: '#474056',
+    },
+    selectColor3: {
+        backgroundColor: '#8A95A5',
+    },
+    selectColor4: {
+        backgroundColor: '#B9C6AE',
+    },
+
+    buttonStart: {
+        position: 'absolute',
+        bottom: 5,
+        marginBottom: "6%",
+        fontSize: 16,
+        fontWeight: '600',
+        color: '#FFFFFF',
+        backgroundColor: '#757083',
+        padding: 20,
+        width: '70%',
+        borderRadius: 10,
+        justifyContent: 'center'
+    },
+    textButton: {
+        fontSize: 18,
+        fontWeight: '600',
+        color: '#FFFFFF',
+        textAlign: 'center',
+    }
+})
+
+export default Start
