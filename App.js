33d511f62e1957cca0c5e0a69a2a812cc9a94cb3
nnnzrnk <127133520+nnnzrnk@users.noreply.github.com>
2023-11-29 15:41:54 +0100

    configurated Firebase

--- App.js
+++ App.js
@@ -1,32 +1,40 @@
-import { StatusBar } from "expo-status-bar";
-import {
-  ScrollView,
-  TextInput,
-  Alert,
-  Button,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
+import {Alert, StyleSheet} from "react-native";
 import { useState } from "react";
 import Start from "./components/Start";
 import Chat from "./components/Chat";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   const [text, setText] = useState("");
 
-  const alertMyText = () => {
-    Alert.alert(text);
-  };
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDwATkzMi397WdtXPu6xu374hKAaGVjiDE",
+    authDomain: "chats-a52f4.firebaseapp.com",
+    projectId: "chats-a52f4",
+    storageBucket: "chats-a52f4.appspot.com",
+    messagingSenderId: "719096245602",
+    appId: "1:719096245602:web:21f059ffaae5119592503b"
+  }
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  // Initialize Cloud Firestore and get a reference to the service
+  const db = getFirestore(app);
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Start">
         <Stack.Screen name="Start" component={Start} />
-        <Stack.Screen name="Chat" component={Chat} />
+        <Stack.Screen name="Chat" >
+        {props => <Chat db={db} {...props} />}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
