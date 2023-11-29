c57e024a699864c45fbd77c21c0a6b15c3259469
nnnzrnk <127133520+nnnzrnk@users.noreply.github.com>
2023-11-29 15:50:25 +0100

    updated to save messages in the database

--- components/Chat.js
+++ components/Chat.js
@@ -1,71 +1,61 @@
 import { useEffect, useState } from "react";
-import { StyleSheet, View, Platform, KeyboardAvoidingView} from "react-native";
-import { Bubble, GiftedChat} from "react-native-gifted-chat";
+import { StyleSheet, View, Platform, KeyboardAvoidingView } from "react-native";
+import { Bubble, GiftedChat } from "react-native-gifted-chat";
+import { collection, query, addDoc, onSnapshot, orderBy } from "firebase/firestore";
 
-const Chat = ({ route, navigation }) => {
-  const {name} = route.params
-  const {background} = route.params;
+const Chat = ({ route, navigation, db }) => {
+  const { name } = route.params
+  const { background } = route.params;
+  const { userID } = route.params
   const [messages, setMessages] = useState([])
 
-  // for sending messages
-  const onSend = (newMessages) => {
-    setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
-  }
+  const onSend = (newMessages) => {addDoc(collection(db, "messages"), newMessages[0])}
 
   // for customazing your messages
   const renderBubble = (props) => {
     return <Bubble
-    {...props}
-    wrapperStyle={ {
-      right: {
-        backgroundColor: '#2a9d8f', 
-      }, 
-      left: {
-        backgroundColor: '#fff'
-      }
-    }}
+      {...props}
+      wrapperStyle={{
+        right: {
+          backgroundColor: '#2a9d8f',
+        },
+        left: {
+          backgroundColor: '#fff'
+        }
+      }}
     />
   }
 
-
   useEffect(() => {
-    navigation.setOptions({title: name})
+    navigation.setOptions({ title: name });
+    const q = query(collection(db, "messages"), orderBy("createdAt", "desc"))
+    const unsubMessages = onSnapshot(q, (documentsSnapshot) => {
+      let newMessages = []
+      documentsSnapshot.forEach(doc => {
+        newMessages.push({ id: doc.id, ...doc.data(), createdAt: new Date(doc.data().createdAt.toMillis())})
+      })
+      setMessages(newMessages)
+    })
+
+    return () => {
+      if (unsubMessages) unsubMessages()
+    }
   }, [])
 
-  // default messages that appear when you get to the chat page
-  useEffect(()=> {
-     setMessages([
-      {
-        _id: 1, 
-        text: 'Hello developer', 
-        createdAt: new Date(), 
-        user: {
-          _id: 2,
-          name: 'React Native', 
-          avatar: 'https://placeimg.com/140/140/any'
-        }, 
-      }, 
-      {
-        _id: 2, 
-        text: 'This is a system message', 
-        createdAt: new Date(), 
-        system: true,
-      }, 
-     ])
-  }, [])
-  
+
   return (
-    <View style={[styles.container, {backgroundColor: background}]}>
+    <View style={[styles.container, { backgroundColor: background }]}>
       <GiftedChat
-      messages={messages}
-      renderBubble={renderBubble}
-      onSend={messages => onSend(messages)}
-      user={{
-        _id: 1
-      }}
+        messages={messages}
+        renderBubble={renderBubble}
+        onSend={messages => onSend(messages)}
+        user={{
+          _id: userID,
+          name: name
+        }}
       />
       {/* so that the keyboard does not overlap the input  */}
-      {Platform.OS === 'android' ? <KeyboardAvoidingView behavior = "height" /> : null}
+      {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
     </View>
   );
 };
